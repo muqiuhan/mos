16c0ef9c4a52da6382528af68d493b08847d7556
Muqiu Han <muqiu-han@outlook.com>
2023-10-19 20:53:43 +0800

    [Update]: Add fuse deps to xmake

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,9 @@
 add_rules("mode.debug", "mode.release")
 
+add_requires({"fuse", system = true})
+
 target("mos")
     set_kind("binary")
     add_cflags("-D_FILE_OFFSET_BITS=64")
     add_files("src/*.c")
+    add_packages("fuse")
