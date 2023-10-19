46ac28eaaf437eb0a9985fcad2df9d55e55f6ce2
Muqiu Han <muqiu-han@outlook.com>
2023-10-19 21:41:57 +0800

    [Update]: Project info

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,15 @@
+set_project("mos")
+set_version("0.0.1")
+set_xmakever("2.8.1")
+
 add_rules("mode.debug", "mode.release")
+add_rules("plugin.compile_commands.autoupdate", {outputdir = "."})
 
 add_requires({"fuse", system = true})
 
 target("mos")
     set_kind("binary")
-    add_cflags("-D_FILE_OFFSET_BITS=64")
-    add_files("src/*.c")
+    set_languages("c++17")
+    add_cxxflags("-D_FILE_OFFSET_BITS=64")
+    add_files("src/*.cpp")
     add_packages("fuse")
